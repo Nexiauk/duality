2ca4323afce528724dc95948bbee3fcc83bdce84
Lucy <savageflames@hotmail.com>
2025-10-13 21:07:48 +0100

    created env.py and added to gitignore. Configured database, secret key and cloudinary in env.py. Updated config in settings.py for databases, secret key, allowed hosts and templates

--- legends/settings.py
+++ legends/settings.py
@@ -11,21 +11,29 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kgj+=u*g0pf!+ji^g*sefcueydy6c%y%p^5!k!eoz13^4sjwx-'
+SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',
+                 ]
 
 
 # Application definition
@@ -73,10 +81,7 @@ WSGI_APPLICATION = 'legends.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
