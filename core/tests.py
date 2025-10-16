3d5ac102a6a17a9e6ca8413ca02df996509108a5
Lucy <savageflames@hotmail.com>
2025-10-16 19:43:03 +0100

    Created a test to ensure that the index page loads successfully. Test passed.

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class CoreTemplatesViewTests(TestCase):
+    def test_index_page_loads_successfully(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "core/index.html")
+        self.assertContains(response, "Test content")
+        
