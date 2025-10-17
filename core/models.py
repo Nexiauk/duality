24f87778e042dbdb82f547fe5764275cf46cac6e
Lucy <savageflames@hotmail.com>
2025-10-17 18:09:32 +0100

    Added Rarity model, ordered by level descending. Defined a string definition and created verbose names for all models in the core app. Updated docstrings.

--- core/models.py
+++ core/models.py
@@ -4,16 +4,17 @@ Defines character information, used across the shop and binder apps.
 Links those characters to rarities, used to calculate prices,
 and archetypes, used for literary references.
 """
-
-from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class CharacterCard(models.Model):
     """
-    Represents different characters that
-    are used across the shop and binder apps.
+    Represents different characters/legends.
+    Base data kept in the database.
+    Extended data kept in a JSON file.
+    Character information used on cards in the shop and
+    binder apps.
 
     Meta:
         Ordering ascending by name.
@@ -51,6 +52,8 @@ class Archetype(models.Model):
 
     Meta:
         Orders ascending by literary_archetype
+        verbose_name: Singular name of the model.
+        verbose_name_plural: Plural name of the model
     """
     literary_archetype = models.CharField(
         _("Literary Archetype"),
@@ -60,7 +63,36 @@ class Archetype(models.Model):
 
     class Meta:
         ordering = ["literary_archetype"]
+        verbose_name = "Archetype"
+        verbose_name_plural = "Archetypes"
 
     def __str__(self):
         """Returns the archetype as its string representation"""
         return self.literary_archetype
+
+
+class Rarity(models.Model):
+    """
+    Represents character card rarities.
+    Rarity is determined by the totalled power stats of a character.
+    It influences the price of cards in the shop and affects styling.
+
+    Meta:
+        Ordered by rarity level descending
+        verbose_name: Singular name of the model.
+        verbose_name_plural: Plural name of the model
+    """
+    name = models.CharField(_("Rarity Name"), max_length=50)
+    level = models.PositiveIntegerField(
+        _("Rarity Level")
+    )
+    price = models.DecimalField(_("Price by Rarity"), max_digits=5, decimal_places=2)
+
+    class Meta:
+        ordering = ["-level"]
+        verbose_name = "Rarity"
+        verbose_name_plural = "Rarities"
+
+    def __str__(self):
+        """Returns the rarity as its string representation"""
+        return self.name
