a8a1ba11cf1b79ee4e8aebe519f91f950d113737
Lucy <savageflames@hotmail.com>
2025-10-18 17:03:15 +0100

    Created archetype model tests, added tests for string method for the rarity model, added tests for string method and ordering for the character card model. Reconfigured file layout to comply with pep8 standards

--- core/tests.py
+++ core/tests.py
@@ -2,62 +2,65 @@ from django.test import TestCase
 from django.urls import reverse
 from core.models import CharacterCard, Archetype, Rarity
 
+
 class CoreTemplatesViewTests(TestCase):
     def test_index_page_loads_successfully(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "core/index.html")
         self.assertContains(response, "Test content")
-        
+
+
 class BaseModelTest(TestCase):
     def setUp(self):
         self.archetype1 = Archetype.objects.create(
-            literary_archetype = "The Wise Mentor",
-            archetype_traits = "Wise, nurturing, patient"
+            literary_archetype="The Wise Mentor",
+            archetype_traits="Wise, nurturing, patient"
         )
 
         self.archetype2 = Archetype.objects.create(
-            literary_archetype = "The Trickster",
-            archetype_traits = "Mysterious, alluring, inconsistent"
+            literary_archetype="The Trickster",
+            archetype_traits="Mysterious, alluring, inconsistent"
         )
 
         self.rarity_mythic = Rarity.objects.create(
-            name = "Mythic",
-            level = 5,
-            price = 29.99
+            name="Mythic",
+            level=5,
+            price=29.99
         )
 
         self.rarity_common = Rarity.objects.create(
-            name = "Common",
-            level = 1,
-            price = 0
+            name="Common",
+            level=1,
+            price=0
         )
 
         self.character_card1 = CharacterCard.objects.create(
-            name = "Iron Man",
-            can_participate_in_rotation = True,
-            archetype = self.archetype1,
-            rarity = self.rarity_mythic
+            name="Iron Man",
+            can_participate_in_rotation=True,
+            archetype=self.archetype1,
+            rarity=self.rarity_mythic
         )
         self.character_card2 = CharacterCard.objects.create(
-            name = "Ant Man",
-            can_participate_in_rotation = False,
-            archetype = self.archetype2,
-            rarity = self.rarity_common
+            name="Ant Man",
+            can_participate_in_rotation=False,
+            archetype=self.archetype2,
+            rarity=self.rarity_common
         )
+
 
 class CharacterCardModelTests(BaseModelTest):
     def test_verbose_name_for_character_card_model_works(self):
-        verbose_name = self.character_card._meta.verbose_name
+        verbose_name = self.character_card1._meta.verbose_name
         self.assertEqual(verbose_name, "Character")
-        
+
     def test_verbose_name_plural_for_character_card_model_works(self):
-        plural_name = self.character_card._meta.verbose_name_plural
+        plural_name = self.character_card1._meta.verbose_name_plural
         self.assertEqual(plural_name, "Characters")
 
     def test_character_name_created_correctly(self):
         self.assertEqual(self.character_card1.name, "Iron Man")
-    
+
     def test_rotation_boolean_works(self):
         self.assertTrue(self.character_card1.can_participate_in_rotation)
 
@@ -65,13 +68,26 @@ class CharacterCardModelTests(BaseModelTest):
         self.assertEqual(self.character_card1.archetype, self.archetype1)
 
     def test_character_archetype_name(self):
-        self.assertEqual(self.character_card1.archetype.literary_archetype, "The Wise Mentor")
+        self.assertEqual(
+            self.character_card1.archetype.literary_archetype,
+            "The Wise Mentor"
+            )
 
     def test_character_has_rarity(self):
         self.assertEqual(self.character_card1.rarity, self.rarity_mythic)
 
     def test_character_rarity_name(self):
         self.assertEqual(self.character_card1.rarity.name, "Mythic")
+
+    def test_character_ordering_works(self):
+        characters = list(CharacterCard.objects.all())
+        expected_order = [self.character_card2, self.character_card1]
+        self.assertEqual(characters, expected_order)
+
+    def test_character_string_method(self):
+        self.assertEqual(self.character_card1.__str__(),
+                         self.character_card1.name)
+
 
 class RarityModelTests(BaseModelTest):
     def test_rarity_name(self):
@@ -95,3 +111,34 @@ class RarityModelTests(BaseModelTest):
         all_rarities = list(Rarity.objects.all())
         expected_order = [self.rarity_mythic, self.rarity_common]
         self.assertEqual(all_rarities, expected_order)
+
+    def test_rarity_string_method(self):
+        self.assertEqual(self.rarity_mythic.__str__(), self.rarity_mythic.name)
+
+
+class ArchetypeModelTests(BaseModelTest):
+    def test_archetype_name(self):
+        self.assertEqual(self.archetype1.literary_archetype, "The Wise Mentor")
+
+    def test_archetype_traits(self):
+        self.assertEqual(self.archetype1.archetype_traits,
+                         "Wise, nurturing, patient")
+
+    def test_verbose_name_singular_for_archetype_model(self):
+        verbose_name = self.archetype1._meta.verbose_name
+        self.assertEqual(verbose_name, "Archetype")
+
+    def test_verbose_name_plural_for_archetype_model(self):
+        verbose_name = self.archetype1._meta.verbose_name_plural
+        self.assertEqual(verbose_name, "Archetypes")
+
+    def test_archetype_ordering_works(self):
+        all_archetypes = list(Archetype.objects.all())
+        expected_order = [self.archetype2, self.archetype1]
+        self.assertEqual(all_archetypes, expected_order)
+
+    def test_archetype_string_method(self):
+        self.assertEqual(
+            self.archetype1.__str__,
+            self.archetype1.literary_archetype
+            )
