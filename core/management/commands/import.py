928f09c6526189d928f8d76cf8fb754d22e4eb0b
Lucy <savageflames@hotmail.com>
2025-10-20 20:05:22 +0100

    Added a docstring to the handle method in import.py. Added try/except failure errors for file loading failure and failure to create the data in the CharacterCard table

--- core/management/commands/import.py
+++ core/management/commands/import.py
@@ -3,6 +3,7 @@ Custom management command to import character data
 from a json file
 """
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError
 from core.models import CharacterCard
 import json
 
@@ -12,12 +13,28 @@ class Command(BaseCommand):
 
     def handle(self):
         """
-        
+        Imports character data from a raw json file into the 
+        CharacterCard model.
+        Reads the JSON file with multiple character objects, each with a unique ID
+        and creates corresponding records in the CharacterCard table.
+
+        Fields in the json file that don't exist in the Django model are ignored.
+        The JSON id field is mapped directly to the CharacterCard
+        Primary Key.
+
+        An error is raised if the file isn't found.
         """
-        with open("legends.json") as json_file:
-            json_data = json.load(json_file)
-            for item in json_data:
-                CharacterCard.objects.get_or_create(
-                    id=item['id'],
-                    name=item['name'],
-                )
+        try:
+            with open("legends.json") as json_file:
+                json_data = json.load(json_file)
+        except (FileNotFoundError, json.JSONDecodeError, PermissionError) as e:
+                raise Exception(f"Failed to load JSON file {e}")
+        try:
+                for item in json_data:
+                    CharacterCard.objects.get_or_create(
+                        id = item['id'],
+                        name = item['name'],
+                    )
+        except (ValueError, IntegrityError) as e:
+             raise Exception(f"Failed to create character card {e}")
+            
