63efa790e457cd53947962217f51d70b2234c79d
Lucy <savageflames@hotmail.com>
2025-10-24 18:51:34 +0100

    Defined a ready method in core/apps.py CoreConfig class, to open the json file so the file is only read once at startup and stores the data in a variable in datastore.py in the data folder, which I initialised with an init file. Changed core app's home view to use the global variable datastore.legends. Tested and all works okay

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+import json
+from pathlib import Path
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        file_path = Path(__file__).parent/"data"/"legends.json"
+        with open(file_path, "r", encoding="utf-8") as json_file:
+            json_data = json.load(json_file)
+            from .data import datastore
+            datastore.legends = json_data
--- core/views.py
+++ core/views.py
@@ -3,20 +3,18 @@ from pathlib import Path
 from .models import CharacterCard
 import random
 import json
+from core.data import datastore
 
 
 def home_view(request):
     page_url = "core/index.html"
     final_list = []
-    file_path = Path(__file__).parent/"data"/"legends.json"
-    with open(file_path, "r", encoding="utf-8") as json_file:
-        json_data = json.load(json_file)
     characters = CharacterCard.objects.all()
     char_list = list(characters)
     res = random.sample(char_list, 3)
     for char in res:
         char_id = char.id
-        for item in json_data:
+        for item in datastore.legends:
             id = item["id"]
             if id == char_id:
                 pair = {
