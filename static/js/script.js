ecc0fbd686a43c399758aaa3d65357f3f5a0296f
Lucy <savageflames@hotmail.com>
2025-11-01 18:08:59 +0000

    Changed loader layout and JS so that only one loader, then all cards appear. Wrapped it in a flex column to center it

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,11 @@
 // After 2 seconds, hides the loaders and displays the cards.
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        const loaders = document.querySelectorAll(".loader");
+        const loader = document.querySelector(".loader");
         const shopcards = document.querySelectorAll(".shop-card");
-
-        loaders.forEach((loader, item) => {
-            loader.style.display = "none";
-            shopcards[item].style.display = "block";
+        loader.style.display = "none";
+        shopcards.forEach(card => {
+            card.style.display = "block";
         })
 
     }, 2000)
