557c73f818da8708344f805ca927aab594972e52
Lucy <savageflames@hotmail.com>
2025-11-02 15:53:02 +0000

    Added formfield_for_foreignkey in ShopScheduleInlineItems in admin.py so that when allocating schedule items to the scheduler, the list only shows characters where rotation=True. Two characters have rotationa s false and on testing, they don't show in the scheduler items lists

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from shop.models import ShopScheduler, ShopScheduleItems
+from core.models import CharacterCard
 
 
 class ShopScheduleItemsInline(admin.TabularInline):
     model = ShopScheduleItems
     extra = 12
+
+    def formfield_for_foreignkey(self, db_field, request, *args, **kwargs):
+        if db_field.name == "character":
+            kwargs["queryset"] = CharacterCard.objects.filter(can_participate_in_rotation=True)
+            return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 @admin.register(ShopScheduler)
 class ShopSchedulerAdmin(admin.ModelAdmin):
