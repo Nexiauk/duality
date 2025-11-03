ea2a73329650757f650892c7ac9b463b3c2239f8
Lucy <savageflames@hotmail.com>
2025-11-03 20:13:01 +0000

    Modified shop view to loop through schedules and if no active schedule, generate a random sample of 12 characters. Added rarity to shop cards via template variable

--- shop/views.py
+++ shop/views.py
@@ -11,6 +11,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from core.data import datastore
 from .models import ShopScheduler
+from core.models import CharacterCard
+import random
 
 
 def shop_view(request):
@@ -38,22 +40,41 @@ def shop_view(request):
         start_time__lte=now,
         end_time__gte=now
     )
-    for schedule in active_schedules:
-        scheduled_items = schedule.scheduled_items.all()
-        for item in scheduled_items:
-            if item.character.can_participate_in_rotation:
-                char_id = item.character.id
-                for legend in datastore.legends:
-                    legend_id = legend["id"]
-                    if legend_id == char_id:
-                        total_power = sum(legend["powerstats"].values())
-                        pair = {
-                            "model": item.character,
-                            "json": legend,
-                            "power": total_power
-                        }
-                        final_list.append(pair)
-    final_list.sort(key=lambda pair: pair["model"].name)
+    if active_schedules:
+        for schedule in active_schedules:
+            scheduled_items = schedule.scheduled_items.all()
+            for item in scheduled_items:
+                if item.character.can_participate_in_rotation:
+                    char_id = item.character.id
+                    char = item.character
+                    for legend in datastore.legends:
+                        legend_id = legend["id"]
+                        if legend_id == char_id:
+                            total_power = sum(legend["powerstats"].values())
+                            pair = {
+                                "model": char,
+                                "json": legend,
+                                "power": total_power
+                            }
+                            final_list.append(pair)
+
+    else:
+        characters = CharacterCard.objects.all()
+        char_list = list(characters)
+        res = random.sample(char_list, 12)
+        for char in res:
+            char_id = char.id
+            for legend in datastore.legends:
+                legend_id = legend["id"]
+                if legend_id == char_id:
+                    total_power = sum(legend["powerstats"].values())
+                    pair = {
+                        "model": char,
+                        "json": legend,
+                        "power": total_power
+                    }
+                    final_list.append(pair)
+    final_list.sort(key=lambda pair: pair["power"], reverse=True)
 
     context = {
         "characters": final_list
