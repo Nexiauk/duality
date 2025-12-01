7976f1c75c6a353e2393f10f0e37452a41a973a1
Lucy <savageflames@hotmail.com>
2025-12-01 15:29:12 +0000

    Setup stripe secret key and added config to settings.py

--- legends/settings.py
+++ legends/settings.py
@@ -26,7 +26,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'os.environ.get("SECRET_KEY")'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
