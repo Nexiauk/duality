fc5667e27ca4c185c395435889d99b6b60911f0d
Lucy <savageflames@hotmail.com>
2025-12-02 20:10:47 +0000

    Added the stripe checkout form to the card-detail page and changed the buy button from an anchor back to a button. Passed through the character id to the view. Created the stripe checkout view and the payment success/payment cancelled views. Hooked up with urls and created a success.html and cancel.html page.

--- shop/urls.py
+++ shop/urls.py
@@ -8,5 +8,8 @@ from . import views
 
 urlpatterns = [
     path("", views.shop_view, name="shop"),
-    path("card-detail/<int:id>", views.card_view, name="card-detail")
+    path("card-detail/<int:id>", views.card_view, name="card-detail"),
+    path("create-checkout-session/<int:id>", views.create_checkout, name="create-checkout-session"),
+    path("success/", views.payment_success, name="payment-success"),
+    path("cancel/", views.payment_cancel, name="payment-cancel"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,10 +6,14 @@ and gathers eligible characters for display. Characters are enriched with
 their data and power status, sorted by power, and rendered to the shop page.
 """
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from .models import ShopScheduler
 from core.models import CharacterCard
 from django.contrib.auth.decorators import login_required
+import stripe
+from django.conf import settings
+from django.urls import reverse
 
 
 def shop_view(request):
@@ -59,3 +63,39 @@ def card_view(request, id):
         "power": power
     }
     return render(request, page_url, context)
+
+stripe.api_key=settings.STRIPE_SECRET_KEY
+
+@login_required
+def create_checkout(request, id):
+    product = get_object_or_404(CharacterCard, id=id)
+    price_in_pence=int(product.rarity.price * 100)
+    json_data=product.get_legends_data()
+    if request.method=='POST':
+        session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price_data':{
+                        'currency': 'gbp',
+                        'product_data': {
+                            'name': product.name,
+                            'images':[json_data['images']['lg']]
+                        },
+                        'unit_amount': price_in_pence,
+                    },
+                    'quantity':1,
+                }
+            ],
+            mode='payment',
+            success_url=request.build_absolute_uri(reverse('payment-success')),
+            cancel_url=request.build_absolute_uri(reverse('payment-cancel'))
+        )
+        return redirect(session.url, code=303)
+    
+def payment_success(request):
+    return render(request, 'shop/success.html')
+
+def payment_cancel(request):
+    return render(request, 'shop/cancel.html')
+
+
