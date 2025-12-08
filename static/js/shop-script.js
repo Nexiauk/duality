8a2c4eaafd1dc18822575b6f107e2a310d2ba353
Lucy <savageflames@hotmail.com>
2025-12-08 21:09:33 +0000

    Added multiple dataset values to each shop card to work with filters. Creates JS for filtering by rarity and wrapped inside the domcontentloaded script that already existed

--- static/js/shop-script.js
+++ static/js/shop-script.js
@@ -1,3 +1,4 @@
+
 // Delay UI swap in the shop: hide loader after 2s, then reveal all shop cards
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
@@ -9,6 +10,24 @@ document.addEventListener("DOMContentLoaded", () => {
         })
 
     }, 2000)
-})
+    // Filtering for the shop
+    const cards = document.querySelectorAll(".shop-card")
+    const Rarity = document.getElementById("rarities")
+    Rarity.addEventListener("change", () => {
+        let selectedRarity = chosenRarity.value
+        cards.forEach((card) => {
+            const cardCol = card.parentNode
+            const dataTags = card.dataset.cardType.toLowerCase()
+            const datatagArray = dataTags.split(" ")
+            if (datatagArray.includes(selectedRarity) || selectedRarity === "all") {
+                card.style.display = "block"
+                cardCol.classList.remove("d-none")
+            }
+            else {
+                card.style.display = "none"
+                cardCol.classList.add("d-none")
+            }
 
-// Filtering for the shop
+        })
+    })
+})
