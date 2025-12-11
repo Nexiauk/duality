c56db1b91aef88a377923a30c878d32b61ce0fac
Lucy <savageflames@hotmail.com>
2025-12-11 20:27:12 +0000

    Created a view to filter and pass through the usercards for the logged-in user, including json data, charactercard reference, power, alignment and universe. Updated binder.html

--- binder/views.py
+++ binder/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Usercards
 
 # Create your views here.
 def binder_view(request):
@@ -8,4 +10,21 @@ def binder_view(request):
     attributes sorted by power.
     """
     page_url = "binder/binder.html"
-    return render(request, page_url)
+    usercards = Usercards.objects.filter(owner=request.user)
+    user_chars = []
+    for char in usercards:
+        character=char.character
+        card_data = {
+            "usercard": char,
+            "chardetails": character,
+            "json": character.get_legends_data(),
+            "power": character.power_status(),
+            "alignment": character.charc_alignment(),
+            "universe": character.charc_universe()
+            }
+        user_chars.append(card_data)
+
+    context = {
+        "characters": user_chars
+    }
+    return render(request, page_url, context)
