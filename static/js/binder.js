f2c60074525dc1425e124b4c3b5a37cc80854ec3
Lucy <savageflames@hotmail.com>
2025-12-17 15:40:43 +0000

    Added javascript for binder card filter display

--- static/js/binder.js
+++ static/js/binder.js
@@ -7,4 +7,35 @@ document.addEventListener("DOMContentLoaded", () => {
             card.style.display = "block";
         })
     }, 3000)
+
+    // Filtering for the shop
+    const cards = document.querySelectorAll(".binder-card")
+    const selects = document.querySelectorAll(".filter")
+    const rarity = document.getElementById("rarities")
+    const alignment = document.getElementById("alignment")
+    const universe = document.getElementById("universe")
+
+    selects.forEach((select) => {
+        select.addEventListener("change", () => {
+            let selectedRarity = rarity.value
+            let selectedAlignment = alignment.value
+            let selectedUniverse = universe.value
+            cards.forEach((card) => {
+                const cardCol = card.parentNode
+                if (
+                    (card.dataset.rarity == selectedRarity || selectedRarity === "all") &&
+                    (card.dataset.alignment == selectedAlignment || selectedAlignment === "all") &&
+                    (card.dataset.universe == selectedUniverse || selectedUniverse === "all")
+                ) {
+                    card.style.display = "block"
+                    cardCol.classList.remove("d-none")
+                }
+                else {
+                    card.style.display = "none"
+                    cardCol.classList.add("d-none")
+                }
+
+            })
+        })
+    })
 })
