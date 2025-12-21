5e85655acda0d2f0d042256750a15f07f7623fe3
Lucy <savageflames@hotmail.com>
2025-12-21 16:47:48 +0000

    Set debug to false after collecting static files and migrating, ready for heroku deployment

---
+++ staticfiles/js/binder.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        const loader = document.querySelector(".loader");
+        const bindercards = document.querySelectorAll(".binder-card");
+        loader.style.display = "none";
+        bindercards.forEach(card => {
+            card.style.display = "block";
+        })
+    }, 3000)
+
+    // Filtering for the shop
+    const cards = document.querySelectorAll(".binder-card")
+    const selects = document.querySelectorAll(".filter")
+    const rarity = document.getElementById("rarities")
+    const alignment = document.getElementById("alignment")
+    const universe = document.getElementById("universe")
+
+    selects.forEach((select) => {
+        select.addEventListener("change", () => {
+            let selectedRarity = rarity.value
+            let selectedAlignment = alignment.value
+            let selectedUniverse = universe.value
+            cards.forEach((card) => {
+                const cardCol = card.parentNode
+                if (
+                    (card.dataset.rarity == selectedRarity || selectedRarity === "all") &&
+                    (card.dataset.alignment == selectedAlignment || selectedAlignment === "all") &&
+                    (card.dataset.universe == selectedUniverse || selectedUniverse === "all")
+                ) {
+                    card.style.display = "block"
+                    cardCol.classList.remove("d-none")
+                }
+                else {
+                    card.style.display = "none"
+                    cardCol.classList.add("d-none")
+                }
+
+            })
+        })
+    })
+})
---
+++ staticfiles/js/shop.js
@@ -0,0 +1,51 @@
+
+// Delay UI swap in the shop: hide loader after 2s, then reveal all shop cards
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        const loader = document.querySelector(".loader");
+        const shopcards = document.querySelectorAll(".shop-card");
+        loader.style.display = "none";
+        shopcards.forEach(card => {
+            card.style.display = "block";
+        })
+
+    }, 3000)
+    // Filtering for the shop
+    const cards = document.querySelectorAll(".shop-card")
+    const selects = document.querySelectorAll(".filter")
+    const rarity = document.getElementById("rarities")
+    const alignment = document.getElementById("alignment")
+    const universe = document.getElementById("universe")
+
+    selects.forEach((select) => {
+        select.addEventListener("change", () => {
+            let selectedRarity = rarity.value
+            let selectedAlignment = alignment.value
+            let selectedUniverse = universe.value
+            cards.forEach((card) => {
+                const cardCol = card.parentNode
+                if (
+                    (card.dataset.rarity == selectedRarity || selectedRarity === "all") &&
+                    (card.dataset.alignment == selectedAlignment || selectedAlignment === "all") &&
+                    (card.dataset.universe == selectedUniverse || selectedUniverse === "all")
+                ) {
+                    card.style.display = "block"
+                    cardCol.classList.remove("d-none")
+                }
+                else {
+                    card.style.display = "none"
+                    cardCol.classList.add("d-none")
+                }
+
+            })
+        })
+    })
+    // A back button that navigates the browser history.
+    document.querySelectorAll(".cancel").forEach(button => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            history.back();
+            console.log("Hello")
+        });
+    });
+})
