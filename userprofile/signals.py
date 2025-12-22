7695b67f292c464c1048d1b8b714d4a64eb637f7
Lucy <savageflames@hotmail.com>
2025-12-22 16:56:35 +0000

    Added transaction on_commit lambda back into signals.py to account for heroku deployment, to ensure a userprofile is only created after a user is fully committed in the database

--- userprofile/signals.py
+++ userprofile/signals.py
@@ -14,9 +14,9 @@ from django.db import transaction
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.get_or_create(
+        transaction.on_commit(lambda: UserProfile.objects.get_or_create(
             user=instance,
-            defaults={'display_name': instance.username}
-        )
+            defaults={'display_name': instance.username, 'bio': 'Hi'}
+        ))
     else:
         UserProfile.objects.get_or_create(user=instance)
